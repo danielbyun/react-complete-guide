d4d53bbc357ad3cc2d12977fa9704afcd7cf6ac4
danielbyun <dbyun92@gmail.com>
2019-08-24 19:57:20 +0900

    Setting state correctly when depending on old state

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -23,7 +23,8 @@ class App extends Component {
     ],
     otherState: "some other value",
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   // runs after the constructor (2)
@@ -64,7 +65,19 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    // this is setting synchronously so is not guaranteed that itll be performed immediately
+    // this.setState({
+    //   persons: persons,
+    //   changeCounter: this.state.changeCounter + 1
+    // });
+
+    // this is setting it and updating immediately
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
   };
 
   deletePersonHandler = personIndex => {
